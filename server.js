5f5cab4dc4186b2bbc198ab454651e2bafc58846
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2023-08-26 20:05:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Client, GatewayIntentBits, Collection }= require('discord.js');
 const fs = require("fs");
 const moment = require('moment');
 const config = require("./config.json");
-const Lava = require("@discordx/lava-player");
+const MoonlinkManager = require("moonlink.js");
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds, 
@@ -15,31 +15,50 @@ const client = new Client({
    ]
   })
                                  
-const node = new Lava.Node({
-  host: {
-    address: process.env.LHOST ?? "",
-    port: Number(process.env.LPORT) ?? 2333,
-  },
-
-  // your Lavalink password
-  password: process.env.LPASS ?? "",
-
-  send(guildId, packet) {
-    const guild = client.guilds.cache.get(guildId);
-    if (guild) {
-      guild.shard.send(packet);
-    }
-  },
-  shardCount: 1, // the total number of shards that your bot is running (optional, useful if you're load balancing)
-  userId: client.user?.id ?? "", // the user id of your bot
+client.moon = new MoonlinkManager([{
+  host: 'localhost',
+  port: 2333,
+  secure: true,
+  password: 'MyPassword'
+}], { /* Option */ }, (guild, sPayload) => {
+  client.guilds.cache.get(guild).shard.send(JSON.parse(sPayload))
+})
+client.moon.on('nodeCreate', (node) => {
+  console.log(`${node.host} was connected`)
+}) //emit to the console the node was connected to
+client.moon.on('trackStart', async(player, track) => {
+  client.channels.cache.get(player.textChannel).send(`${track.title} is playing now`) //when the player starts it will send a message to the channel where the command was executed
+})
+client.moon.on('trackEnd', async(player, track) => {
+  client.channels.cache.get(player.textChannel).send(`track is over`) //when the player starts it will send a message to the channel where the command was executed
+})
+client.on('ready', () => {
+  client.moon.init(client.user.id); //initializing the package
 });
+client.on('raw', (data) => {
+  client.moon.packetUpdate(data) //this will send to the package the information needed for the package to work properly
+})
+client.on('message', async(message) => {
+  if (message.content.startsWith('f!play'){
+  if (!message.member.voice.channel) return message.reply({
+    content: `you are not on a voice channel`,
+    ephemeral: true
+  })
+  
+  let player = client.moon.players.create({
+    guildId: message.guild.id,
+    voiceChannel: message.member.voice.channel.id,
+    textChannel: message.channel.id,
+    autoPlay: true
+  }); //creating a player
+  if (!player.connected) player.connect({
+    setDeaf: true,
+    setMute: false
+  }) // if the player is not connected it will connect to the voice channel
 
-client.ws.on("VOICE_STATE_UPDATE", ({data: Lava.VoiceStateUpdate}) => {
-  node.voiceStateUpdate(data);
-});
-
-client.ws.on("VOICE_SERVER_UPDATE", ({data: Lava.VoiceServerUpdate}) => {
-  node.voiceServerUpdate(data);
+  if (!player.playing) player.play()
+  }
+}
 });
 
 client.commands = new Collection();
