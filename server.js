e224e453e30c5fb25abc9f2e992ab908103f52c7
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2023-09-01 20:41:50 +0700

    Update new with slash command

--- server.js
+++ server.js
@@ -1,48 +1,32 @@
-//code start here
-//import all modules
-const { Client, GatewayIntentBits, Collection }= require('discord.js');
-const fs = require("fs");
-const moment = require('moment');
-const config = require("./config.json");
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds, 
-    GatewayIntentBits.GuildMembers, 
-    GatewayIntentBits.GuildMessages, 
-    GatewayIntentBits.MessageContent, 
-    GatewayIntentBits.GuildVoiceStates
-   ]
-})
-
-client.commands = new Collection();
-client.events = new Collection();
-client.aliases = new Collection();
-client.config = config;
-client.queue = new Collection()
-
-//Command Handler
-fs.readdir("./commands/", (err, files) => {
-if (err) console.error(err);
-    files.forEach(f => {
-        let props = require(`./commands/${ f }`);
-        props.fileName = f;
-        client.commands.set(props.help.name, props);
-        props.help.aliases.forEach(alias => {
-            client.aliases.set(alias, props.help.name);
-        });
-    });
-})
-
-//events handler
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-    delete require.cache[require.resolve(`./events/${file}`)];
-  });
+	intents: [
+		GatewayIntentBits.Guilds, 
+		GatewayIntentBits.GuildMessages, 
+		GatewayIntentBits.GuildPresences, 
+		GatewayIntentBits.GuildMessageReactions, 
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.MessageContent
+	], 
+	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction] 
 });
 
-client.login(process.env.TOKEN);
+const fs = require('fs');
+const config = require('./config.json');
+require('dotenv').config() // remove this line if you are using replit
+
+client.commands = new Collection()
+client.aliases = new Collection()
+client.slashCommands = new Collection();
+client.buttons = new Collection();
+client.prefix = config.prefix;
+
+module.exports = client;
+
+
+fs.readdirSync('./handlers').forEach((handler) => {
+  require(`./handlers/${handler}`)(client)
+});
+
+
+client.login(process.env.TOKEN)
