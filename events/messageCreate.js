2a2131181a46d1485ed3da11a7c1ba30a22f2b4c
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2023-09-01 20:45:21 +0700

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,17 +1,62 @@
-module.exports = (client, message) => {
-  
-  try {
-  if (message.author.bot) return;
-  if (message.content.indexOf(client.config.prefix) !== 0) return;
-  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-  let command = args.shift().toLowerCase();
-  if(message.channel.type === "dm") return;
-    
-  if (client.aliases.has(command)) command = client.commands.get(client.aliases.get(command)).help.name
+const { EmbedBuilder, Collection, PermissionsBitField } = require('discord.js')
+const ms = require('ms');
+const client = require('..');
+const config = require('../config.json');
 
-  let commandFile = require(`../commands/${command}.js`);
-  commandFile.run(client, message, args);
-} catch (err) {
-  console.error(err)
-}
- }
+const prefix = client.prefix;
+const cooldown = new Collection();
+
+client.on('messageCreate', async message => {
+	if(message.author.bot) return;
+	if(message.channel.type !== 0) return;
+	if(!message.content.startsWith(prefix)) return; 
+	const args = message.content.slice(prefix.length).trim().split(/ +/g); 
+	const cmd = args.shift().toLowerCase();
+	if(cmd.length == 0 ) return;
+	let command = client.commands.get(cmd)
+	if(!command) command = client.commands.get(client.aliases.get(cmd));
+	
+	if(command) {
+		if(command.cooldown) {
+				if(cooldown.has(`${command.name}${message.author.id}`)) return message.channel.send({ content: config.messages["COOLDOWN_MESSAGE"].replace('<duration>', ms(cooldown.get(`${command.name}${message.author.id}`) - Date.now(), {long : true}) ) });
+				if(command.userPerms || command.botPerms) {
+					if(!message.member.permissions.has(PermissionsBitField.resolve(command.userPerms || []))) {
+						const userPerms = new EmbedBuilder()
+						.setDescription(`🚫 ${message.author}, You don't have \`${command.userPerms}\` permissions to use this command!`)
+						.setColor('Red')
+						return message.reply({ embeds: [userPerms] })
+					}
+					if(!message.guild.members.cache.get(client.user.id).permissions.has(PermissionsBitField.resolve(command.botPerms || []))) {
+						const botPerms = new EmbedBuilder()
+						.setDescription(`🚫 ${message.author}, I don't have \`${command.botPerms}\` permissions to use this command!`)
+						.setColor('Red')
+						return message.reply({ embeds: [botPerms] })
+					}
+				}
+
+				command.run(client, message, args)
+				cooldown.set(`${command.name}${message.author.id}`, Date.now() + command.cooldown)
+				setTimeout(() => {
+					cooldown.delete(`${command.name}${message.author.id}`)
+				}, command.cooldown);
+			} else {
+				if(command.userPerms || command.botPerms) {
+					if(!message.member.permissions.has(PermissionsBitField.resolve(command.userPerms || []))) {
+						const userPerms = new EmbedBuilder()
+						.setDescription(`🚫 ${message.author}, You don't have \`${command.userPerms}\` permissions to use this command!`)
+						.setColor('Red')
+						return message.reply({ embeds: [userPerms] })
+					}
+				
+					if(!message.guild.members.cache.get(client.user.id).permissions.has(PermissionsBitField.resolve(command.botPerms || []))) {
+						const botPerms = new EmbedBuilder()
+						.setDescription(`🚫 ${message.author}, I don't have \`${command.botPerms}\` permissions to use this command!`)
+						.setColor('Red')
+						return message.reply({ embeds: [botPerms] })
+					}
+			}
+			command.run(client, message, args)
+		}
+	}
+	
+});
