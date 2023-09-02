98cc8faf74ae387db66779f3ee41a9ef961caee2
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2023-09-03 01:19:55 +0700

    Update setup.js

--- slashCommands/utility/setup.js
+++ slashCommands/utility/setup.js
@@ -62,10 +62,10 @@ module.exports = {
         );
 
         collector.on('collect', async i=> {
-            await i.update({ content: 'Selected! Fetching the detailed data relating to your choices...', components: [], embeds:[] });
+            await i.reply({ content: 'Selected! Fetching the detailed data relating to your choices...', components: [], embeds:[] });
             await wait(3150); // wait 3.15 secs to emulate the delay of the network
-            await i.editReply({content: "Here's your detailed descriptions related to your choices!", embeds: [], components: []});
-
+            await i.reply({content: "Here's your detailed descriptions related to your choices!", embeds: [], components: []}).delete(1000);
+    
 		const value = Array.from(i.values);
             if (value.includes('mole')) {
                 await i.followUp({content: "", embeds: [embed1], components: []});
