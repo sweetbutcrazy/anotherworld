763c3e1891f299a951328f140b621474783a6e67
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2023-09-04 21:08:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const client = new Client({
 });
 
 const fs = require('fs');
+const Welcomer = require('./Welcomer.js');
 const config = require('./config.json');
 require('dotenv').config() // remove this line if you are using replit
 
@@ -28,6 +29,23 @@ module.exports = client;
 fs.readdirSync('./handlers').forEach((handler) => {
   require(`./handlers/${handler}`)(client)
 });
+
+client.on("guildMemberAdd", async member => {
+    const image = new Welcomer()
+    .setBackground("https://i.pinimg.com/originals/07/28/dc/0728dc400eca09632215055ff003d8bf.gif")
+    .setGIF(true)
+    .setAvatar(member.user.displayAvatarURL({ format: "png" }))
+    .setName(member.user.username)
+    .setDiscriminator(member.user.discriminator)
+    .setBlur(2)
+
+    const channel = await member.guild.channels.fetch()
+    .then(channels => channels.find(x => x.id === ""))
+
+    return channel?.send({
+        files: [ new MessageAttachment(await image.generate(), "welcome.gif") ]
+    })
+})
 
 client.on("error", (e) => console.error(e));
 client.on("warn", (e) => console.warn(e));
