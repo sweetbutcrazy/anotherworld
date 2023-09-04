3e3ae944cb5de0fe2877b5db0ecfba5fa6d72049
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2023-09-04 07:43:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -29,32 +29,6 @@ fs.readdirSync('./handlers').forEach((handler) => {
   require(`./handlers/${handler}`)(client)
 });
 
-client.on("guildMemberAdd", (member) => {
-  
-/*  const myIntents = new IntentsBitField();
-  myIntents.add(IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildMembers);
-  */
-  const Channel = member.guild.channels.cache.get("719871302445563974")//.Permissions({intents: [myIntents]})
-  const embed = new EmbedBuilder()
-    .setDescription(`Welcome to ***Another World🌎***
-U have chance to get **Nitro Boost** for free` + '\n' + 
-`Check it out:` + '\n' +
-`<#1142671893443387412>` + '\n' + 
-`Read The Rules In :` + '\n' +
-`<#1065237028788252712>` + '\n' +
-`Take Your Role In :` + '\n' + 
-`<#1126799549747437649>` + '\n' +
-`Introduction Yourself In :` + '\n' +
-`<#1077305891717652500>`)
- // .setDescription(`Read The Rules In :` + '\n' + `<#1065237028788252712>` + '\n' + `Take Your Role In :` + '\n' + `<#1126799549747437649>` + '\n' + `Introduction Yourself In :` + '\n' + `<#1077305891717652500>`)    
-  .setThumbnail(member.user.avatarURL())
-  .setImage('https://cdn.discordapp.com/attachments/596041860711972864/1143712429906333827/27343902-1dec-4650-bba8-28b7843a3e3f.jpg') 
-  .setFooter({text: `${member.guild.name}`, iconURL: member.guild.iconURL()})
-  .setTimestamp()
-  Channel.send({content:`Welcome ${member.user}`, embeds: [embed]}).catch(console.error);
-
-})
-
 client.on("error", (e) => console.error(e));
 client.on("warn", (e) => console.warn(e));
 client.on("debug", (e) => console.info(e));
