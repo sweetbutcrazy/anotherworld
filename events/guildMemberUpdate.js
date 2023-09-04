bb358c700f148ec300583a9add7320ce474836d9
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2023-09-04 22:07:32 +0700

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,10 +1,30 @@
-
+const { Permissions, EmbedBuilder, IntentsBitField } = require("discord.js");
 const client = require('..');
 
 
 client.on("guildMemberUpdate", async(newMember, oldMember) => {
-  
+  if (!oldMember.roles.cache.size !== newMember.roles.cache.size) {
+      if (
+        !oldMember.roles.cache.has(
+          newMember.guild.roles.premiumSubscriberRole.id
+        ) &&
+        newMember.roles.cache.has(
+          newMember.guild.roles.premiumSubscriberRole.id
+        )
+      ) {
+        const chnl = newMember.guild.channels.cache.get('1125953407442755664');
+        const boostEmbed = new EmbedBuilder()
+          .setDescription(
+            `> <@${newMember.user.id}>.\n\n> Thanks For Boost The Server \n> u Can DM STAFF to Make a Custom Role`
+          )
+          .setColor("F47FFF")
+          .setFooter({
+            text: `${newMember.guild.name}`,
+            iconURL: newMember.guild.iconURL({ size: 1024 }),
+          })
+          .setTimestamp();
 
+        chnl.send({content: newMember.user.tag, embed: boostEmbed })
 
   
 }
